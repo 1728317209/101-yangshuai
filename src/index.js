559cd41dee3fa7cdbdf7383386eb2346bf0dd65b
unknown <1728317209@qq.com>
2018-08-20 11:59:45 +0800

    08.20

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import { PersistGate } from 'redux-persist/es/integration/react';
 import configureStore from './store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 import AppRoutes from './routes';
 
-const { store, persistor } = configureStore();
+const store = configureStore();
 
 render(
   <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Router history={browserHistory}>
-        {AppRoutes()}
-      </Router>
-    </PersistGate>
+    <Router history={browserHistory}>
+      {AppRoutes()}
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,20 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore, persistCombineReducers } from 'redux-persist';
 import { createLogger } from 'redux-logger';
-import storage from 'redux-persist/es/storage';
 import rootReducer from '../reducers';
 import serverApi from '../middleware/serverApi';
 
-const config = {
-  key: 'root',
-  storage
-};
-const reducer = persistCombineReducers(config, rootReducer);
-
 const configureStore = preloadedState => {
   const store = createStore(
-    reducer,
+    rootReducer,
     preloadedState,
     applyMiddleware(thunk, serverApi, createLogger())
   );
@@ -25,9 +17,8 @@ const configureStore = preloadedState => {
       store.replaceReducer(rootReducer);
     });
   }
-  const persistor = persistStore(store);
 
-  return { store, persistor };
+  return store;
 };
 
 export default configureStore;
